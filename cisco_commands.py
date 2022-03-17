389b91974c5fc9d72798bb6c95239ce928d6fcb7
RFAlias <jared.stevenson@outlook.com>
2022-03-17 13:39:00 -0400

    Update cisco_commands.py

--- cisco_commands.py
+++ cisco_commands.py
@@ -1,11 +1,6 @@
 import paramiko
 import time
 
-#Setup base connection, put it in a function probably
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('69.164.202.83', username='root', key_filename='./.ssh/id_rsa')
-connection = ssh.invoke_shell()
 
 # Send commands, takes an ssh.invoke_shell object and
 # a list of commands to be run. \r\n is sent after each command
@@ -22,5 +17,12 @@ def send_command(connection, commands):
         lbuff += buff
     return lbuff
 
-data = send_command(connection, ["ls /etc","ls /root"])
-print(data)
+
+if __name__ == "__main__":
+    #Setup base connection, put it in a function probably
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect('69.164.202.83', username='root', key_filename='./.ssh/id_rsa')
+        connection = ssh.invoke_shell()
+        data = send_command(connection, ["cd /etc", "ls", 'cd /tmp', "touch made_from_python"])
+        print(data)
